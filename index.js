61f640774cca6a6040b7a866be8ce795d135ad0a
obayed324 <23-52090-2@student.aiub.edu>
2025-12-07 10:27:00 +0600

    Server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+require('dotenv').config();
+const port = process.env.PORT || 3000
+
+
+// middleware
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Sarker is shifting shifting')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
